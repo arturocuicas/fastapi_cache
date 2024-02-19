39ae1c94c160fa4fe06076a8c22736d57dc5c54d
Ulysses Oliveira <102372800+ulysses-lorde@users.noreply.github.com>
2024-02-19 13:41:21 -0300

    Atualizações de execução do projeto (#1)
    * algumas instruções de como usar a aplicação
    
    * add uma segunda alternativa para import da clas GlobalConfig
    
    * arquivos atualizados. foi adicionado a biblioteca sqlalchemy-utils
    
    * função create_db_and_tables refatorada, antes não criava o banco de dados como sugere o nome

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FastAPI Cache
 Cache in FastAPI
+Projeto Original
+
+## Executar aplicação
+docker-compose build
+docker-compose up
+
+## Acessar banco de dados
+docker-compose up -d
+apt install postgresql-client
+psql -h localhost -p 5434 -U postgres -d nome_do_banco_de_dados
--- app/db/sessions.py
+++ app/db/sessions.py
@@ -1,6 +1,7 @@
 from faker import Faker
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
 from sqlmodel import SQLModel, create_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -24,6 +25,15 @@ async_session = sessionmaker(
 
 
 async def create_db_and_tables() -> None:
+    '''Verifica se o banco de dados existe,
+    caso não exista, cria-o'''
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+
+
+async def reset_tables() -> None:
     SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
 
